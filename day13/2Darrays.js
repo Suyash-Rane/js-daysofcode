c5483a4600eb3664fe759f838d75fdfe07ac8431
CursedGandhi <suyash3695@gmail.com>
2023-01-15 13:33:04 +0530

    DAY 13

---
+++ day13/2Darrays.js
@@ -0,0 +1,44 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+
+    let arr = Array(6);
+
+    for (let i = 0; i < 6; i++) {
+        arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+    var hg = -Infinity;
+    var mhg = -Infinity;
+    for(let i = 0; i < 4; i++)
+    {
+        for(let j = 0; j < 4; j++)
+        {
+            hg = arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
+            if(mhg<hg)
+                mhg = hg;
+        }
+    }
+    console.log(mhg);
+}
