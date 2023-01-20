979deefdc78810484bd29fea12c759ab11e986db
CursedGandhi <suyash3695@gmail.com>
2023-01-20 13:20:06 +0530

    DAY 17

---
+++ day17/stringtoint.js
@@ -0,0 +1,34 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    const S = readLine();
+    try
+    {
+        
+        console.log(eval(S));
+    }
+    catch(e)
+    {
+        console.log("Bad String")
+    }
+}
